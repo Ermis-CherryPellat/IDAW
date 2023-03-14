32dc965730ee505f926ee0c3af266acecd44145f
Ermis Cherry-Pellat <ermis.cherrypellat@gmail.com>
2023-03-14 09:49:57 +0100

    v2 exo3

---
+++ SitePro/v2/index.php
@@ -0,0 +1,51 @@
+        <?php
+        require_once('template_header.php');
+        ?>
+
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top shadow-sm" id="mainNav">
+            <div class="container px-5">
+                <a class="navbar-brand fw-bold" href="index.php">TP1 IDAW</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="bi-list"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto me-4 my-3 my-lg-0">
+                        <li class="nav-item" id="currentpage"><a class="nav-link me-lg-3" href="index.php">Accueil</a></li>
+                        <li class="nav-item"><a class="nav-link me-lg-3" href="cv.php">CV</a></li>
+                        <li class="nav-item"><a class="nav-link me-lg-3" href="projets.php">Projets</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Mashead header-->
+        <header class="masthead">
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-12">
+                        <!-- Mashead text and app badges-->
+                        <div class="mb-5 mb-lg-0 text-center text-lg-start">
+                            <h1 class="display-1 lh-1 mb-3">Bonjour</h1>
+                            <p class="lead fw-normal text-muted mb-5">Je suis un professionnel</p>
+                            <p class="lead fw-normal text-muted mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus integer feugiat scelerisque varius morbi enim nunc faucibus. Id diam vel quam elementum. Mauris sit amet massa vitae tortor condimentum lacinia. Nibh sit amet commodo nulla facilisi nullam vehicula ipsum. Commodo nulla facilisi nullam vehicula ipsum a arcu cursus. Bibendum at varius vel pharetra vel turpis nunc. Viverra aliquet eget sit amet. Ut venenatis tellus in metus. Pulvinar etiam non quam lacus suspendisse faucibus interdum. At volutpat diam ut venenatis tellus. Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices. Turpis egestas pretium aenean pharetra.</p>
+                            <p class="lead fw-normal text-muted mb-5">Bibendum enim facilisis gravida neque convallis. Tempor id eu nisl nunc. Justo eget magna fermentum iaculis eu non diam phasellus vestibulum. Aenean euismod elementum nisi quis. Placerat duis ultricies lacus sed turpis tincidunt id aliquet. Rutrum quisque non tellus orci ac auctor augue mauris augue. In metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Ac tortor vitae purus faucibus ornare suspendisse sed nisi. Justo nec ultrices dui sapien. Amet venenatis urna cursus eget nunc.</p>
+                            <p class="lead fw-normal text-muted mb-5">Vulputate sapien nec sagittis aliquam malesuada bibendum arcu. Sit amet consectetur adipiscing elit. Quis ipsum suspendisse ultrices gravida dictum fusce. Eu sem integer vitae justo eget magna fermentum. Neque convallis a cras semper auctor. Pharetra massa massa ultricies mi quis hendrerit dolor magna eget. Enim blandit volutpat maecenas volutpat blandit aliquam etiam erat. Neque viverra justo nec ultrices dui sapien. Suspendisse ultrices gravida dictum fusce ut placerat orci nulla pellentesque. Quis ipsum suspendisse ultrices gravida. Nisi quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus. Neque egestas congue quisque egestas. Porta lorem mollis aliquam ut porttitor leo a diam sollicitudin. Quis auctor elit sed vulputate mi. Est lorem ipsum dolor sit amet consectetur adipiscing.</p>
+                            <p class="lead fw-normal text-muted mb-5">Erat imperdiet sed euismod nisi porta lorem mollis. Scelerisque fermentum dui faucibus in ornare. Scelerisque in dictum non consectetur. Sit amet nulla facilisi morbi tempus iaculis urna. Nulla posuere sollicitudin aliquam ultrices. Ullamcorper eget nulla facilisi etiam dignissim diam. Lectus urna duis convallis convallis tellus. Malesuada fames ac turpis egestas sed tempus urna et pharetra. Mattis rhoncus urna neque viverra justo nec ultrices dui. Purus non enim praesent elementum facilisis. Convallis aenean et tortor at risus viverra adipiscing. Cras adipiscing enim eu turpis egestas. Purus in mollis nunc sed id semper risus in hendrerit. Amet nulla facilisi morbi tempus iaculis. Ut diam quam nulla porttitor massa id neque. Malesuada fames ac turpis egestas integer eget aliquet nibh. Sit amet consectetur adipiscing elit pellentesque habitant morbi tristique senectus. Nisi lacus sed viverra tellus in hac. Vitae purus faucibus ornare suspendisse sed.</p>
+                            <p class="lead fw-normal text-muted mb-5">Sagittis purus sit amet volutpat. Et ultrices neque ornare aenean. Adipiscing commodo elit at imperdiet dui accumsan. Donec massa sapien faucibus et molestie ac feugiat sed. Aenean et tortor at risus viverra adipiscing. Amet facilisis magna etiam tempor orci eu lobortis elementum. Sit amet commodo nulla facilisi. Erat nam at lectus urna duis convallis convallis. Et netus et malesuada fames ac. Dui accumsan sit amet nulla facilisi morbi tempus. Odio aenean sed adipiscing diam donec adipiscing tristique risus. Posuere lorem ipsum dolor sit.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        
+        <!-- Footer-->
+        <footer class="bg-black text-center py-5">
+            <div class="container px-5">
+                <div class="text-white-50 small">
+                    <div class="mb-2">&copy; TP1 IDAW Mars 2022. All Rights Reserved.</div>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
---
+++ SitePro/v2/template_header.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+   <head>
+    <title>Ermis Cherry-Pellat</title>
+    <!-- <link rel="stylesheet" href="style1.css" type="text/css" media="screen" title="default" charset="utf-8" /> -->
+    <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Google fonts-->
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Newsreader:ital,wght@0,600;1,600&amp;display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,300;0,500;0,600;0,700;1,300;1,500;1,600;1,700&amp;display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,400;1,400&amp;display=swap" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="startbootstrap-new-age-master/dist/css/styles.css" rel="stylesheet" />
+        <!-- My CSS theme -->
+        <link href="myStyle.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
