d88d1513a619318f0b320e9e9122a8a8962bc547
Ermis Cherry-Pellat <ermis.cherrypellat@gmail.com>
2023-03-14 09:52:54 +0100

    v2 exo3 fini

--- SitePro/v2/projets.php
+++ SitePro/v2/projets.php
@@ -40,13 +40,6 @@
             </div>
         </header>
         
-        <!-- Footer-->
-        <footer class="bg-black text-center py-5">
-            <div class="container px-5">
-                <div class="text-white-50 small">
-                    <div class="mb-2">&copy; TP1 IDAW Mars 2022. All Rights Reserved.</div>
-                </div>
-            </div>
-        </footer>
-    </body>
-</html>
+        <?php
+        require_once('template_footer.php');
+        ?>
---
+++ SitePro/v2/template_footer.php
@@ -0,0 +1,10 @@
+ <!-- Footer-->
+ <footer class="bg-black text-center py-5">
+            <div class="container px-5">
+                <div class="text-white-50 small">
+                    <div class="mb-2">&copy; TP1 IDAW Mars 2022. All Rights Reserved.</div>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
