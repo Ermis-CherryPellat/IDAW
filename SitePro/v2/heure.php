d22922a4149caebd5b3bc3765fd94db158ac996b
Ermis Cherry-Pellat <ermis.cherrypellat@gmail.com>
2023-03-14 09:37:16 +0100

    programme qui donne l'heure

---
+++ SitePro/v2/heure.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Page qui donne l'heure</title>
+    </head>
+    <body>
+        
+    <p>La date de today is :<?php echo date('l jS \of F Y h:i:s A') ?></p>
+        
+    </body>
+</html>
