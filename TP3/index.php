fd216bb2489445da9a2bc82ba15fb8f4ae13ad33
Ermis Cherry-Pellat <ermis.cherrypellat@gmail.com>
2023-03-17 09:29:18 +0100

    exo 1 tp3

---
+++ TP3/index.php
@@ -0,0 +1,13 @@
+<!doctype html> <html>
+<head>
+<meta charset="utf-8">
+<title>Hello World Wild Web</title>
+</head> <body>
+<h1>I am there!</h1> </body>
+<?php
+    require_once("login.php");
+    // require_once("connected.php");
+
+?>
+</html>
+
