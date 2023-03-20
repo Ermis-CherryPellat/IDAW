6de4c8a5c3150a5a697fdf47e06200be6c6f9ded
Ermis Cherry-Pellat <ermis.cherrypellat@gmail.com>
2023-03-20 12:03:29 +0100

    exo 3 pas fini

---
+++ tp4/exo3/config.php
@@ -0,0 +1,13 @@
+<?php
+define('_MYSQL_HOST','127.0.0.1');
+define('_MYSQL_PORT',8889);
+define('_MYSQL_DBNAME','dbtest');
+define('_MYSQL_USER','root');
+define('_MYSQL_PASSWORD','root');
+
+$db_host = '127.0.0.1'; // Hôte de la base de données
+$db_name = 'dbtest'; // Nom de la base de données
+$db_user = 'root'; // Nom d'utilisateur de la base de données
+$db_pass = 'root'; // Mot de passe de la base de données
+
+?>
---
+++ tp4/exo3/users.php
@@ -0,0 +1,63 @@
+<?php
+    require_once('config.php');
+    require_once('db_init.php');    
+
+    // requête SQL pour récupérer les utilisateurs
+    $request = $pdo->prepare("select * from users");
+    $request->execute();
+    $users = $request->fetchAll(PDO::FETCH_OBJ);
+
+    // affichage des utilisateurs dans un tableau HTML
+    if (count($users) > 0) {
+      echo "<table>";
+      echo "<tr><th>ID</th><th>Nom</th><th>Adresse email</th></tr>";
+      foreach ($users as $user) {
+        echo "<tr>";
+        echo "<td>".$user->id_user."</td>";
+        echo "<td>".$user->nom."</td>";
+        echo "<td>".$user->email."</td>";
+        
+        echo "</tr>";
+      }
+      echo "</table>";
+    } else {
+      echo "Aucun utilisateur trouvé.";
+    }
+    
+    // echo'<div class="container">
+	
+    //     <div class="table">
+    //         <div class="table-header">
+    //             <div class="header__item">User Id</div>
+    //             <div class="header__item">Name</div>
+    //             <div class="header__item">Email</div>
+    //         </div>';
+
+    // echo '
+    //         <div class="table-content">	
+    //             <div class="table-row">		
+    //                 <div class="table-data">Tom</div>
+    //                 <div class="table-data">2</div>
+    //                 <div class="table-data">0</div>
+    //             </div>
+    //             <div class="table-row">
+    //                 <div class="table-data">Paul</div>
+    //                 <div class="table-data">1</div>
+    //                 <div class="table-data">1</div>
+    //             </div>
+    //             <div class="table-row">
+    //                 <div class="table-data">Harry</div>
+    //                 <div class="table-data">0</div>
+    //                 <div class="table-data">2</div>
+    //             </div>
+    //         </div>	
+    //     </div>
+    // </div>
+    // <p>Bonjourin</p>';
+   
+    
+
+    $pdo = null;
+
+?>
+
