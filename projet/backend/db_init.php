4e075d3fe5101da43028be87d3f90f0fb8a61065
RodoIanbou <97095045+RodoIanbou@users.noreply.github.com>
2023-04-02 16:19:18 +0200

    changement variable mot de passe

--- projet/backend/db_init.php
+++ projet/backend/db_init.php
@@ -15,29 +15,3 @@ try {
 } catch (PDOException $erreur) {
     echo 'Erreur : '.$erreur->getMessage();
 }
-try {
-    // Connexion à la base de données
-    // $pdo = new PDO($connectionString,_MYSQL_USER,_MYSQL_PASSWORD,$options);
-    // $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    // // Supprimer la base de données si elle existe
-    // $pdo->exec("DROP DATABASE IF EXISTS $db_name");
-
-    // // Créer la base de données
-    // $pdo->exec("CREATE DATABASE $db_name");
-
-    // // Sélectionner la base de données
-    // $pdo->exec("USE $db_name");
-
-    // // Importer la structure de la base de données depuis un fichier SQL
-    // $sql_structure = file_get_contents('dbtest.sql');
-    // $pdo->exec($sql_structure);
-
-    // Importer les données de test depuis un fichier SQL
-    // $sql_data = file_get_contents('dbtest.sql');
-    // $pdo->exec($sql_data);
-
-    // echo "Base de données créée et initialisée avec succès !";
-} catch(PDOException $e) {
-    echo "Erreur : " . $e->getMessage();
-}
--- projet/frontend/footer.php
+++ projet/frontend/footer.php
@@ -1,13 +1,10 @@
   <!-- ======= Footer ======= -->  
   <footer id="footer" class="footer">
     <div class="copyright">
-      &copy; Sité créé par  <strong><span>Ermis CherryPellat & Rodolphe Ianboukhtine </span></strong>
+      &copy; Sité créé par  <strong><span>Ermis CherryPellat & Rodolphe Ianboukhtine | IMT Nord Europe </span></strong>
     </div>
     <div class="credits">
-      <!-- All the links in the footer should remain intact. -->
-      <!-- You can delete the links only if you purchased the pro version. -->
-      <!-- Licensing information: https://bootstrapmade.com/license/ -->
-      <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
+
     </div>
   </footer><!-- End Footer -->
  
