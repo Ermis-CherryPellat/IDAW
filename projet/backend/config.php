f0ac46fab81552a39c54153f76f993d198d8fbe2
Ermis Cherry-Pellat <ermis.cherrypellat@gmail.com>
2023-04-07 20:15:28 +0200

    api nutriment

--- projet/backend/config.php
+++ projet/backend/config.php
@@ -2,19 +2,19 @@
 
 // ERMIS //
 
-// define('_MYSQL_HOST','127.0.0.1');
-// define('_MYSQL_PORT',8889);
-// define('_MYSQL_DBNAME','projetIMM');
-// define('_MYSQL_USER','root');
-// define('_MYSQL_PASSWORD','root');
+define('_MYSQL_HOST','127.0.0.1');
+define('_MYSQL_PORT',8889);
+define('_MYSQL_DBNAME','projetIMM');
+define('_MYSQL_USER','root');
+define('_MYSQL_PASSWORD','root');
 
 
 // RODOLPHE //
 
-define('_MYSQL_HOST','127.0.0.1');
-define('_MYSQL_PORT',3306);
-define('_MYSQL_DBNAME','projetimm');
-define('_MYSQL_USER','root');
-define('_MYSQL_PASSWORD','');
+// define('_MYSQL_HOST','127.0.0.1');
+// define('_MYSQL_PORT',3306);
+// define('_MYSQL_DBNAME','projetimm');
+// define('_MYSQL_USER','root');
+// define('_MYSQL_PASSWORD','');
 
 
